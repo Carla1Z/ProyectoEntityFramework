e2e4a2c3f0e7518530a1f31632b1ee26e02bf38f
Carla1Z <carla_sabrina_zarate@hotmail.com>
2023-03-28 15:18:09 -0300

    Utilizando base de datos en memoria

--- ProyectoEntityFramework/Program.cs
+++ ProyectoEntityFramework/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProyectoEntityFramework;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbconexion", async ([FromServices] TareasContext dbContext) =>
+{
+    dbContext.Database.EnsureCreated();
+    return Results.Ok("Base de datos en memoria: " + dbContext.Database.IsInMemory());
+
+});
+
+app.Run();
