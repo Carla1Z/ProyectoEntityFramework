2cea5c0db4ec1254f3737d7579e9e29c52ceeb36
Carla1Z <carla_sabrina_zarate@hotmail.com>
2023-03-28 14:05:33 -0300

    Configuración de Entity Framework

---
+++ ProyectoEntityFramework/TareasContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoEntityFramework.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoEntityFramework
+{
+    public class TareasContext : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Tarea> Tareas { get; set; }
+
+        public TareasContext(DbContextOptions<TareasContext> options) : base(options) { }
+
+    }
+}
